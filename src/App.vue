c21cf23eaaffbc119e9ebfe2cd16e15b44699823
Elise Griset <elise.griset@gmail.com>
2018-06-25 12:32:31 +0200

    yo

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view/>
+           <footer> Made with 🍭 by Elise Griset</footer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Teste Vue Posts',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #e9bf00;
+  color: white;
+  text-align: center;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,29 @@
+<template>
+        <div>
+          <h2>Welcome to my CRUD Test Website </h2>
+          <h1>MEVN App</h1>
+        <router-link v-bind:to="{ name: 'Posts' }" class="">See all Posts</router-link>
+      </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
